88250b87ed1c3fbc84afe3f9507a312eba262242
Arian Sayah <arian.sk@gmail.com>
2014-03-21 12:18:50 +0100

    V2

--- src/aufgabe7/HelloWorld.java
+++ src/aufgabe7/HelloWorld.java
@@ -7,4 +7,7 @@ public class HelloWorld {
 	public static void helloMond(){
 		System.out.println("Hello Mond");
 	}
+	public static void helloStefan(){
+		System.out.println("Hello Mond"); //ZUM MOND
+	}
 }
