65d1c483a5dbb7c73fd44807ffa3868a6e17e0cd
Arian Sayah <arian.sk@gmail.com>
2014-03-21 10:41:47 +0100

    Zweites Update

--- src/aufgabe7/HelloWorld.java
+++ src/aufgabe7/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
 	public static void helloWorld(){
 		System.out.println("Hello World");
 	}
-	
+	public static void helloMond(){
+		System.out.println("Hello Mond");
+	}
 }
